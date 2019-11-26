acc38553996142420bfc15f5717b68fcf9ca5acd
sira27 <sira27jkt@gmail.com>
2019-11-26 10:52:39 +0700

    Menambahkan form Data Pribadi Dosen

--- personal.php
+++ personal.php
@@ -16,6 +16,83 @@
                     <li class="active">Data Pribadi Dosen</li>
                 </ol>
             </section>
+            <section class="content">
+                <div class="jumbotron bg-light-blue text-center">
+                    <h2>DATA PRIBADI DOSEN</h2>
+                </div>
+                <form action="#" class="form-horizontal">
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Nama Lengkap</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="terapan">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Gelar</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="terapan">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">NIDK</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="terapan">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Alamat Surel</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="terapan">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Nomor Handphone</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="terapan">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Bidang Keahlian</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" rows="1" id="textarea"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row text-right">
+                        <div class="col-sm-12">
+                            <button class="btn btn-primary" onclick="">+ Tambah</button>
+                            <button class="btn btn-primary" onclick="">- Kurang</button>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-2">
+                            <label class="control-label" for="terapan">Jabatan Akademik</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6">
+                            <button type="submit" class="btn btn-primary">< Kembali</button>
+                        </div>
+                        <div class="col-sm-6 text-right">
+                            <button type="submit" class="btn btn-primary">Sunting Data</button>
+                        </div>
+                    </div>
+                </form>
+            </section>
         </div>
     </div>
     <?php include 'frame2.php';?>
