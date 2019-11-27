0ffa8b5f9e10d24f045fbde5b7fc018be32a1e5c
hzalkarim <hafizh.alkarim02@gmail.com>
2019-11-27 21:30:26 +0700

    Menambahkan tabel

--- bimbingan.php
+++ bimbingan.php
@@ -16,6 +16,42 @@
                     <li class="active">Bimbingan Skripsi</li>
                 </ol>
             </section>
+            <section class="content">
+                <div class="text-right"><button class="btn btn-success btn-sm" data-toggle="modal" data-target="#save"><i class="fa fa-plus"></i></button></div>
+                    <table class="table table-bordered col-lg-12">
+                        <thead class="bg-aqua-gradient">
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Dosen</th>
+                                <th>Judul Tugas Akhir</th>
+                                <th>Nama Mahasiswa</th>
+                                <th>Tahun</th>
+                                <th>Program Studi</th>
+                                <th>File</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Namaku dosen</td>
+                                <td>Kuakhiri dengan Hamdalah</td>
+                                <td>Senang</td>
+                                <td>1945</td>
+                                <td>Comp Sains</td>
+                                <td class="text-center" style="max-width: 30px">
+                                    <button class="btn btn-primary">Info</button>
+                                </td>
+                                <td class="text-center" style="max-width: 90px">
+                                    <button class="btn btn-warning" data-toggle="modal" data-target="#edit">Edit</button>
+                                    <button class="btn btn-danger" data-toggle="modal" data-target="#delete">Delete</button>
+                                    <button class="btn btn-success">Print</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
         </div>
     </div>
     <?php include 'frame2.php';?>
