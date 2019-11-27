b0d489de48094d649b6edf0c3f97daf984323ae6
H_Alkarim <hafizh.alkarim@gmail.com>
2019-11-27 17:20:45 +0700

    Menambahkan modal delete

--- writing.php
+++ writing.php
@@ -48,17 +48,38 @@
                             </th>
                             <th class="text-center" style="max-width: 120px">
                                 <button class="btn btn-warning">Edit</button>
-                                <button class="btn btn-danger">Delete</button>
+                                <button class="btn btn-danger" data-toggle="modal" data-target="#delete">Delete</button>
                                 <button class="btn btn-success">Print</button>
                             </th>
                         </tr>
                     </tbody>
                 </table>
             </section>
+
+            <!--Delete modal-->
+            <div div class="modal fade" id="delete">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header bg-red">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Delete Data</h4>
+                        </div>
+                        <form action="#">
+                            <input name="id" type="hidden" value="">
+                            <div class="modal-body">
+                                <p>Apakah anda yakin menghapus data ini?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </div>
+                        </form>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.example-modal -->
         </div>
     </div>
     <?php include 'frame2.php';?>
 </body>
 <?php include 'footer.php';?>
-
 </html>
