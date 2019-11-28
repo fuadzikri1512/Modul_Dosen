df20689975a13789f545620c9f576d148942b072
hzalkarim <hafizh.alkarim02@gmail.com>
2019-11-28 11:49:53 +0700

    Penambahan modal delete dan save (modal sudah lengkap)

--- pendidikan.php
+++ pendidikan.php
@@ -55,7 +55,7 @@
                 <div class="modal-dialog modal-lg">
                     <!-- Modal content-->
                     <div class="modal-content">
-                        <div class="modal-header bg-green text-center">
+                        <div class="modal-header bg-yellow text-center">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h4 class="modal-title">SUNTING DATA PENDIDIKAN DOSEN</h4>
                         </div>
@@ -107,12 +107,98 @@
                             </form>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Batalkan</button>
-                            <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+                            <button type="button" class="btn" data-dismiss="modal">Batalkan</button>
+                            <button type="submit" class="btn btn-warning">Simpan Perubahan</button>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!--Add Modal-->
+            <div id="save" class="modal fade" role="dialog">
+                <div class="modal-dialog modal-lg">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header bg-green text-center">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">INSERT/ADD PENDIDIKAN DOSEN</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form action="#" class="form-horizontal">
+                                <div class="form-group row">
+                                    <div class="col-lg-2">
+                                        <label class="control-label" for="terapan">Magister Terapan</label>
+                                    </div>
+                                    <div class="col-lg-10">
+                                        <input type="text" class="form-control" id="terapan">
+                                    
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-2">
+                                        <label class="control-label" for="terapan">Perguruan Tinggi</label>
+                                    </div>
+                                    <div class="col-lg-10">
+                                        <input type="text" class="form-control" id="perguruan-tinggi">
+                                    
+                                    </div>
+                                </div>
+                                <div class="form-group row has-feedback">
+                                    <div class="col-lg-2 col-xs-12">
+                                        <label for="tahun" class="control-label">Tahun</label>
+                                    </div>
+                                    <div class="col-lg-3 col-xs-5">
+                                        <input type="date" id="tahun-awal" class="form-control">
+                                        <span class="glyphicon glyphicon-calendar form-control-feedback"></span>
+                                    </div>
+                                    <div class="col-lg-1 col-xs-2">
+                                        <label for="" class="control-label">sampai</label>
+                                    </div>
+                                    <div class="col-lg-3 col-xs-5">
+                                        <input type="date" id="tahun-akhir" class="form-control">
+                                        <span class="glyphicon glyphicon-calendar form-control-feedback"></span>
+                                    </div>
+                                </div>
+                                <!--div class="form-group row">
+                                    <div class="col-xs-4">
+                                        <button type="submit" class="btn btn-primary">Kembali</button>
+                                    </div>
+                                    <div class="text-right col-xs-8">
+                                        <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#sunting-pend-dosen" >Tambahkan Data</button>
+                                        <button type="submit" class="btn btn-primary">Sunting Data</button>
+                                    </div>
+                                </div-->
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn" data-dismiss="modal">Batalkan</button>
+                            <button type="submit" class="btn btn-success">Simpan Perubahan</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Delete modal-->
+            <div class="modal fade" id="delete">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header bg-red">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Delete Data</h4>
+                        </div>
+                        <form action="#">
+                            <input name="id" type="hidden" value="">
+                            <div class="modal-body">
+                                <p>Apakah anda yakin menghapus data ini?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </div>
+                        </form>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.example-modal -->
         </div>
     </div>
     <?php include 'frame2.php';?>
