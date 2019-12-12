bbcc3b0f114849ba6ff9ae0fa73d4b0388658208
fuadzikri15 <muhammadfuadzikri2@gmail.com>
2019-12-12 20:14:59 +0700

    config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "modul_dosen";
+
+$con = mysqli_connect($server, $username, $password) or die("Koneksi gagal");
+mysqli_select_db($con, $database) or die("Database tidak bisa dibuka");
+?>
